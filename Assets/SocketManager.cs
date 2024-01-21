ea60b27203974fe8b44371f1b3cc10431745ba3f
SungyeopJeong <syeopj4a@gmail.com>
2024-01-21 15:26:20 +0900

    feat: join by mobile

--- Assets/SocketManager.cs
+++ Assets/SocketManager.cs
@@ -2,11 +2,29 @@ using System;
 using System.Collections.Generic;
 using SocketIOClient;
 using UnityEngine;
+using UnityEngine.UI;
+using Newtonsoft.Json;
+using TMPro;
+
+[Serializable]
+class JoinData
+{
+    public string name;
+    public int statusCode;
+
+    public JoinData(string name)
+    {
+        this.name = name;
+    }
+}
 
 public class SocketManager : MonoBehaviour
 {
     public ServerConfig serverConfig;
     public SocketIOUnity socket;
+    public TextMeshProUGUI player1, player2;
+    short playerN = 0;
+    JoinData[] player = new JoinData[2];
 
     void Start()
     {
@@ -25,6 +43,53 @@ public class SocketManager : MonoBehaviour
             Debug.Log("Connected");
         };
         socket.OnDisconnected += (sender, e) => { Debug.Log("Disconnected: " + e); };
+        
+    }
+
+    public void OnReady(string code)
+    {
+        socket.OnUnityThread("ready " + code, (response) =>
+        {
+            JoinData data = JsonUtility.FromJson<JoinData>(response.ToString().Trim('[', ']'));
+            if (data.statusCode == 201)
+            {
+                if (playerN == 0)
+                {
+                    player[0] = new JoinData(data.name);
+                    player1.text = data.name;
+                    playerN++;
+                }
+                else if (playerN == 1)
+                {
+                    if (player[0] == null)
+                    {
+                        player[0] = new JoinData(data.name);
+                        player1.text = data.name;
+                    }
+                    else
+                    {
+                        player[1] = new JoinData(data.name);
+                        player2.text = data.name;
+                    }
+                    playerN++;
+                }
+            }
+            else if (data.statusCode == 200)
+            {
+                if (player[0] != null && player[0].name == data.name)
+                {
+                    player[0] = null;
+                    player1.text = "Player 1";
+                    playerN--;
+                }
+                else if (player[1] != null && player[1].name == data.name)
+                {
+                    player[1] = null;
+                    player2.text = "Player 2";
+                    playerN--;
+                }
+            }
+        });
     }
 
 
